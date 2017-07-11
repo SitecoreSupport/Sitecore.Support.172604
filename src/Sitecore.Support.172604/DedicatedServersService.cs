dc3ab921913f857b6dc79851732327381f7a99d4
Ivan Sheyenko <ivs@sitecore.net>
2017-07-11 15:08:47 +0300

    patch created

---
+++ src/Sitecore.Support.172604/DedicatedServersService.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Modules.EmailCampaign.Messages;
+using Sitecore.Modules.EmailCampaign.Core.Services;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.Services
+{
+  public class DedicatedServersService : Sitecore.Modules.EmailCampaign.Core.Services.DedicatedServersService
+  {
+    public override int Status(MessageItem message, out int totalNumServers)
+    {
+      if (message.State == MessageState.Sent)
+      {
+        totalNumServers = DedicatedServers.GetServers().Count;
+        return 0;
+      }
+
+      return base.Status(message, out totalNumServers);
+    }
+  }
+}
---
+++ src/Sitecore.Support.172604/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.172604")]
+[assembly: AssemblyProduct("Sitecore.Support.172604")]
+[assembly: ComVisible(false)]

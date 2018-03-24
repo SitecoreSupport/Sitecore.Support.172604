a7cb3a8a84f2e1b122fb8c3263e9c8caef499fa7
DK\anr <anr@sitecore.net>
2018-03-24 14:20:16 +0200

    code style fix

--- src/Sitecore.Support.172604/Modules/EmailCampaign/Core/Services/DedicatedServersService.cs
+++ src/Sitecore.Support.172604/Modules/EmailCampaign/Core/Services/DedicatedServersService.cs
@@ -3,11 +3,11 @@ using Sitecore.Modules.EmailCampaign.Core.Services;
 
 namespace Sitecore.Support.Modules.EmailCampaign.Core.Services
 {
-  public class DedicatedServersService : Sitecore.Modules.EmailCampaign.Core.Services.DedicatedServersService
-  {
-    public override int Status(MessageItem message, out int totalNumServers)
+    public class DedicatedServersService : Sitecore.Modules.EmailCampaign.Core.Services.DedicatedServersService
     {
-      return base.Status(message, out totalNumServers);
+        public override int Status(MessageItem message, out int totalNumServers)
+        {
+            return base.Status(message, out totalNumServers);
+        }
     }
-  }
 }
